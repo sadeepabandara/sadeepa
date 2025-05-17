92ea442ad566ff9a3c34005eb88db2a3b4a497b6
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-05-18 09:47:34 +1000

    change next.config.js file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
