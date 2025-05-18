cc94bd2ea0d7d4f3ce31c3a2c7ec382ffb16bfe6
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-05-19 09:54:09 +1000

    add clarity

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,17 @@ function MyApp({ Component, pageProps }) {
                 `}
             </Script>
 
+            {/* Microsoft Clarity */}
+            <Script id="ms-clarity" strategy="afterInteractive">
+                {`
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rlqawwhr7d");
+        `}
+            </Script>
+
             <Layout>
                 <AnimatePresence mode="wait">
                     <motion.div key={router.route} className="h-full">
