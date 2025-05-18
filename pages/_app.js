7a8fe88e2b6b5330e83307632d821658fe185031
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-05-19 09:34:01 +1000

    add GA4

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,38 @@ import Layout from "../components/Layout";
 import Transition from "../components/Transition";
 import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter();
 
     return (
-        <Layout>
-            <AnimatePresence mode="wait">
-                <motion.div key={router.route} className="h-full">
-                    <Transition />
-                    <Component {...pageProps} />
-                </motion.div>
-            </AnimatePresence>
-        </Layout>
+        <>
+            {/* Google Analytics */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-MP69DSCG62"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-MP69DSCG62', {
+                        page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
+            <Layout>
+                <AnimatePresence mode="wait">
+                    <motion.div key={router.route} className="h-full">
+                        <Transition />
+                        <Component {...pageProps} />
+                    </motion.div>
+                </AnimatePresence>
+            </Layout>
+        </>
     );
 }
 
